cae5a48ee22e438a40f27ffd756b9d132a13f80e
Cristina Zhang <73481741+CristinaZhangchan@users.noreply.github.com>
2025-08-08 14:28:04 +0200

    Update parameters_generate.py

--- code/parameters_generate.py
+++ code/parameters_generate.py
@@ -5,13 +5,13 @@ parameters={}
 parameters['dwi_channel_num']=13
 parameters['dce_channel_num']=6
 
-parameters['dwi_tensordata']=r'D:\DeepLearning\data\dwi_tensordata.pth'
-parameters['dce_tensordata']=r'D:\DeepLearning\data\dce_tensordata.pth'
-parameters['labels_tensordata']=r'D:\DeepLearning\data\labels_tensordata.pth'
+parameters['dwi_tensordata']=r'/kaggle/input/breastcaner-subtypes/dwi_tensordata'
+parameters['dce_tensordata']=r'/kaggle/input/breastcaner-subtypes/dce_tensordata'
+parameters['labels_tensordata']=r'/kaggle/input/breastcaner-subtypes/labels_tensordata'
 
-parameters['dwi_test_tensordata']=r'D:\DeepLearning\data\dwi_test_tensordata.pth'
-parameters['dce_test_tensordata']=r'D:\DeepLearning\data\dce_test_tensordata.pth'
-parameters['labels_test_tensordata']=r'D:\DeepLearning\data\labels_test_tensordata.pth'
+parameters['dwi_test_tensordata']=r'/kaggle/input/breastcaner-subtypes/dwi_test_tensordata'
+parameters['dce_test_tensordata']=r'/kaggle/input/breastcaner-subtypes/dce_test_tensordata'
+parameters['labels_test_tensordata']=r'/kaggle/input/breastcaner-subtypes/labels_test_tensordata/labels_test_tensordata.pth'
 
 parameters['device'] = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -22,9 +22,10 @@ parameters['input_size']=32
 parameters['segnum']=5
 parameters['classnum']=4
 
-torch.save(parameters,r"D:\DeepLearning\data\parameters.pth")
+torch.save(parameters,r"/kaggle/input/breastcaner-subtypes/parameters")
 
 model_dict={}
-torch.save(model_dict,r"D:\DeepLearning\data\model_dict.pth")
+torch.save(model_dict,r"/kaggle/input/breastcaner-subtypes/model_dict/model_dict.pth")
 fusion_model_dict={}
-torch.save(fusion_model_dict,r"D:\DeepLearning\data\fusion_model_dict.pth")
+
+torch.save(fusion_model_dict,r"/kaggle/input/breastcaner-subtypes/fusion_model_dict/fusion_model_dict.pth")
