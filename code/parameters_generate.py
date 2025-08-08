594a334f66d1bc6a3dfbdecc413cbc059efe863f
Cristina Zhang <73481741+CristinaZhangchan@users.noreply.github.com>
2025-08-08 16:31:47 +0200

    Update parameters_generate.py

--- code/parameters_generate.py
+++ code/parameters_generate.py
@@ -5,13 +5,13 @@ parameters={}
 parameters['dwi_channel_num']=13
 parameters['dce_channel_num']=6
 
-parameters['dwi_tensordata']=r'/kaggle/input/breastcaner-subtypes/dwi_tensordata/dwi_tensordata.pth'
-parameters['dce_tensordata']=r'/kaggle/input/breastcaner-subtypes/dce_tensordata/dce_tensordata.pth'
-parameters['labels_tensordata']=r'/kaggle/input/breastcaner-subtypes/labels_tensordata/labels_tensordata.pth'
+parameters['dwi_tensordata']=r'/kaggle/working/breastcaner-subtypes/dwi_tensordata/dwi_tensordata.pth'
+parameters['dce_tensordata']=r'/kaggle/working/breastcaner-subtypes/dce_tensordata/dce_tensordata.pth'
+parameters['labels_tensordata']=r'/kaggle/working/breastcaner-subtypes/labels_tensordata/labels_tensordata.pth'
 
-parameters['dwi_test_tensordata']=r'/kaggle/input/breastcaner-subtypes/dwi_test_tensordata/dwi_test_tensordata.pth'
-parameters['dce_test_tensordata']=r'/kaggle/input/breastcaner-subtypes/dce_test_tensordata/dce_test_tensordata.pth'
-parameters['labels_test_tensordata']=r'/kaggle/input/breastcaner-subtypes/labels_test_tensordata/labels_test_tensordata.pth'
+parameters['dwi_test_tensordata']=r/kaggle/working/breastcaner-subtypes/dwi_test_tensordata/dwi_test_tensordata.pth'
+parameters['dce_test_tensordata']=r'/kaggle/working/breastcaner-subtypes/dce_test_tensordata/dce_test_tensordata.pth'
+parameters['labels_test_tensordata']=r'/kaggle/working/breastcaner-subtypes/labels_test_tensordata/labels_test_tensordata.pth'
 
 parameters['device'] = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -22,11 +22,12 @@ parameters['input_size']=32
 parameters['segnum']=5
 parameters['classnum']=4
 
-torch.save(parameters,r"/kaggle/input/breastcaner-subtypes/parameters/parameters.pth")
+torch.save(parameters,r"/kaggle/working/breastcaner-subtypes/parameters/parameters.pth")
 
 model_dict={}
-torch.save(model_dict,r"/kaggle/input/breastcaner-subtypes/model_dict/model_dict.pth")
+torch.save(model_dict,r"/kaggle/working/breastcaner-subtypes/model_dict/model_dict.pth")
 fusion_model_dict={}
 
-torch.save(fusion_model_dict,r"/kaggle/input/breastcaner-subtypes/fusion_model_dict/fusion_model_dict.pth")
+torch.save(fusion_model_dict,r"/kaggle/working/breastcaner-subtypes/fusion_model_dict/fusion_model_dict.pth")
+
 
